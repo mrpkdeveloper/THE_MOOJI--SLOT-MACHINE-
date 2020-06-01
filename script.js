5b0dff9690fde60b749afc38a69b3d2f2b7af0da
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-01 20:07:25 +0530

    spin functionality added

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ let value1 = document.getElementById('value-1')
 let value2 = document.getElementById('value-2')
 let value3 = document.getElementById('value-3')
 // let idspeed = document.getElementById('idspeed')
-let Stop_bttn = document.getElementById('stop-bttn')
-let Start_bttn = document.getElementById('start-bttn')
+// let Stop_bttn = document.getElementById('stop-bttn')
+// let Start_bttn = document.getElementById('start-bttn')
+let Spin_bttn = document.getElementById('spin-bttn')
 let result_bttn = document.getElementById('result')
 
 let values = [
     '😁', '😃', '😎', '😇', '😅', '😠', '😨',
-    '😕', '😁', '😁', '😁', '😁', '😁', '😁'
+    '😕', '😁', '😁', '😁', '😁', '😁', '😁',
+    '😕', '😁', '😁', '😁', '😁', '😁', '😁',
+    '😕', '😁', '😁', '😁', '😁', '😁', '😁',
 ]
 
 function randomvalues() {
@@ -34,13 +37,14 @@ function updateanimation(newspeed) {
 //     updateanimation(ev.target.value)
 // }
 
-Start_bttn.onclick = function () {
-    document.documentElement.style.setProperty('--speed', 5)
-    updateanimation(5)
-    result_bttn.innerText = "Result"
-}
+// Start_bttn.onclick = function () {
+//     document.documentElement.style.setProperty('--speed', 5)
+//     updateanimation(5)
+//     result_bttn.innerText = "Result"
+// }
 
-Stop_bttn.onclick = function () {
+// Stop_bttn.onclick = function () {
+function stop() {
     document.documentElement.style.setProperty('--speed', 0)
     clearInterval(animateid) //face change stop
     if (value1.innerText == value2.innerText && value1.innerText == value3.innerText) {
@@ -48,4 +52,14 @@ Stop_bttn.onclick = function () {
     } else {
         result_bttn.innerText = "U LOST"
     }
-} 
+}
+
+
+Spin_bttn.onclick = function () {
+    result_bttn.innerText = "Result"
+    document.documentElement.style.setProperty('--speed', 5)
+    updateanimation(5)
+    setTimeout(() => {
+        stop()
+    }, 1200)
+}
