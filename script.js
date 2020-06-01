e6d49dd0338593b6386c3f301fa1559ab02c50c0
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-01 19:31:24 +0530

    start stop functionality added

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let value1 = document.getElementById('value-1')
 let value2 = document.getElementById('value-2')
 let value3 = document.getElementById('value-3')
-let idspeed = document.getElementById('idspeed')
-let Start_bttn = document.getElementById('bttn')
+// let idspeed = document.getElementById('idspeed')
+let Stop_bttn = document.getElementById('stop-bttn')
+let Start_bttn = document.getElementById('start-bttn')
 
 let values = [
     '😁', '😃', '😎', '😇', '😅', '😠', '😨', '😕', '😁', '😁', '😁', '😁', '😁', '😁'
@@ -25,14 +26,25 @@ function updateanimation(newspeed) {
     }, 1000 / newspeed)
 }
 
-idspeed.onchange = function (ev) {
-    // document.documentElement => this is root of css
-    document.documentElement.style.setProperty('--speed', ev.target.value)
-    updateanimation(ev.target.value)
-}
+// idspeed.onchange = function (ev) {
+//     // document.documentElement => this is root of css
+//     document.documentElement.style.setProperty('--speed', ev.target.value)
+//     updateanimation(ev.target.value)
+// }
 
 Start_bttn.onclick = function () {
-    console.log("clicked")
-    clearInterval(animateid) //face change stop
-    document.documentElement.style.setProperty('--speed', 0)
+    document.documentElement.style.setProperty('--speed', 5)
+    updateanimation(5)
 }
+
+Stop_bttn.onclick = function () {
+    document.documentElement.style.setProperty('--speed', 0)
+    clearInterval(animateid) //face change stop
+    if (value1.innerText == value2.innerText && value1.innerText == value3.innerText) {
+        console.log("u win")
+        window.alert("u win")
+    } else {
+        console.log("u loose")
+        window.alert("u loose")
+    }
+} 
